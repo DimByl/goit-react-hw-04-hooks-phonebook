535297b1cf2ea22ca4a6ef5d8422461c5b712cac
Dmytro Bykanov <75586266+DimByl@users.noreply.github.com>
2021-10-01 00:01:19 +0300

    up2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from "./logo.svg";
-import "./App.css";
+import { useState } from "react";
+import shortid from "shortid";
+import styles from "./App.scss";
+import ContactForm from "./components/ContactForm/ContactForm";
+import ContactList from "./components/ContactList/ContactList";
+import Filter from "./components/Filter/Filter";
+import Container from "./components/Container/Container";
 
-function App() {
+const App = () => {
+  const [contacts, setContacts] = useState([]);
+  const [filter, setFilter] = useState("");
+
+  const addNewContact = (name, number) => {
+    const contact = {
+      id: shortid.generate(),
+      name,
+      number,
+    };
+
+    if (!name || !number) {
+      alert("Please enter the correct name and number");
+      return;
+    }
+
+    contacts.find(({ name }) => name === contact.name)
+      ? alert(`${name} is already in contacts`)
+      : setContacts((prevState) => [contact, ...prevState]);
+  };
+
+  const handleChangeFilter = (event) => setFilter(event.currentTarget.value);
+
+  const getContactsToShow = () =>
+    contacts.filter(({ name }) =>
+      name.toLowerCase().includes(filter.toLowerCase())
+    );
+
+  const handleDeleteContact = (id) =>
+    setContacts((prevState) =>
+      prevState.filter((contact) => contact.id !== id)
+    );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <h1 className={styles.title}>Phonebook (hook)</h1>
+      <ContactForm onSubmit={addNewContact} />
+
+      <h2 className={styles.titleContacts}>Contacts</h2>
+      <Filter value={filter} onChangeFilter={handleChangeFilter} />
+      <ContactList
+        contacts={getContactsToShow()}
+        onDeleteContact={handleDeleteContact}
+      />
+    </Container>
   );
-}
+};
 
 export default App;
