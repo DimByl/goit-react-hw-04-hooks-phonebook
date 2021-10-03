a5030989c128f7c1e841c692ece7ad784f7dd8e2
Dmytro Bykanov <75586266+DimByl@users.noreply.github.com>
2021-10-04 01:45:41 +0300

    up3

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import { useState } from "react";
-import shortid from "shortid";
+import { useState, useEffect } from "react";
 import styles from "./App.scss";
 import ContactForm from "./components/ContactForm/ContactForm";
 import ContactList from "./components/ContactList/ContactList";
@@ -10,29 +9,29 @@ const App = () => {
   const [contacts, setContacts] = useState([]);
   const [filter, setFilter] = useState("");
 
-  const addNewContact = (name, number) => {
-    const contact = {
-      id: shortid.generate(),
-      name,
-      number,
-    };
-
-    if (!name || !number) {
-      alert("Please enter the correct name and number");
-      return;
+  useEffect(() => {
+    const savedContacts = JSON.parse(localStorage.getItem("contacts"));
+    if (savedContacts) {
+      setContacts(savedContacts);
     }
+  }, []);
 
-    contacts.find(({ name }) => name === contact.name)
-      ? alert(`${name} is already in contacts`)
-      : setContacts((prevState) => [contact, ...prevState]);
-  };
+  useEffect(() => {
+    localStorage.setItem("contacts", JSON.stringify(contacts));
+  }, [contacts]);
+
+  const addNewContact = (contact) =>
+    setContacts((prevState) => [contact, ...prevState]);
 
   const handleChangeFilter = (event) => setFilter(event.currentTarget.value);
 
-  const getContactsToShow = () =>
-    contacts.filter(({ name }) =>
-      name.toLowerCase().includes(filter.toLowerCase())
+  const getContactsToShow = () => {
+    const normalizedFilter = filter.toLowerCase();
+
+    return contacts.filter(({ name }) =>
+      name.toLowerCase().includes(normalizedFilter)
     );
+  };
 
   const handleDeleteContact = (id) =>
     setContacts((prevState) =>
@@ -42,7 +41,7 @@ const App = () => {
   return (
     <Container>
       <h1 className={styles.title}>Phonebook (hook)</h1>
-      <ContactForm onSubmit={addNewContact} />
+      <ContactForm contacts={contacts} onSubmit={addNewContact} />
 
       <h2 className={styles.titleContacts}>Contacts</h2>
       <Filter value={filter} onChangeFilter={handleChangeFilter} />
--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,63 +1,90 @@
 import { useState } from "react";
 import PropTypes from "prop-types";
-
+import shortid from 'shortid';
 import styles from "./ContactForm.module.scss";
+import PhoneInput from 'react-phone-number-input';
+import 'react-phone-number-input/style.css';
 
-const ContactForm = ({ onSubmit }) => {
-  const [state, setState] = useState({
-    name: "",
-    number: "",
-  });
-
-  const { name, number } = state;
+const ContactForm = ({ contacts, onSubmit }) => {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
 
   const handleChange = (event) => {
     const { name, value } = event.currentTarget;
-    setState({ ...state, [name]: value });
+    if (name === 'name') {
+      setName(value);
+    }
   };
 
   const handleFormSubmit = event => {
     event.preventDefault();
 
-    onSubmit(name, number);
+    if (!name || !number) {
+      alert('Please enter the correct name and number');
+      return;
+    }
+
+    const contact = {
+      id: shortid.generate(),
+      name,
+      number,
+    };
+
+    const checkSameName = contacts.find(({ name }) => name === contact.name);
+    const checkSameNumber = contacts.find(
+      ({ number }) => number === contact.number,
+    );
+
+    if (checkSameNumber) {
+      const { name, number } = checkSameNumber;
+      alert(`This number already exists: "${name}: ${number}"`);
+      return;
+    }
+
+    if (checkSameName) {
+      alert(`${name} is already in contacts`);
+      return;
+    }
+
+    onSubmit(contact);
     reset();
   };
 
   const reset = () => {
-    setState({ name: '', number: '' });
+    setName('');
+    setNumber('');
   };
 
-    return (
-      <form className={styles.ContactForm} onSubmit={handleFormSubmit}>
-        <label className={styles.formLabel}>
-          <span className={styles.formText}>Name</span>
-          <input
-            className={styles.formInput}
-            type="text"
-            placeholder="Enter contact's name"
-            name="name"
-            value={name}
-            onChange={handleChange}
-          />
-        </label>
-        <label className={styles.formLabel}>
-          <span className={styles.formText}>Number</span>
-          <input
-            className={styles.formInput}
-            type="tel"
-            placeholder="Enter contact's number"
-            name="number"
-            value={number}
-            onChange={handleChange}
-          />
-        </label>
+  return (
+    <form className={styles.ContactForm} onSubmit={handleFormSubmit}>
+      <label className={styles.formLabel}>
+        <span className={styles.formText}>Name</span>
+        <input
+          className={styles.formInput}
+          type="text"
+          placeholder="Enter contact's name"
+          name="name"
+          value={name}
+          onChange={handleChange}
+        />
+      </label>
+      <label className={styles.formLabel}>
+        <span className={styles.formText}>Number</span>
+        <PhoneInput
+          value={number}
+          onChange={setNumber}
+          defaultCountry="UA"
+          international
+          autoComplete="off"
+        />
+      </label>
 
-        <button className={styles.formBtn} type="submit">
-          Add contact
-        </button>
-      </form>
-    );
-  }
+      <button className={styles.formBtn} type="submit">
+        Add contact
+      </button>
+    </form>
+  );
+};
 
 ContactForm.propTypes = {
   onSubmit: PropTypes.func.isRequired,
